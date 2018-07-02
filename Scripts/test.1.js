935277b59d2992e91da45c72a29503cbafcce795
Doc <derosier.matt@gmail.com>
2018-07-02 14:11:00 -0500

    Bump

--- Scripts/test.1.js
+++ Scripts/test.1.js
@@ -1,10 +1,9 @@
+/*---- GLOBAL VARIABLES ----*/
+
 let _stopdropchecker = 0; //default 0; used to prevent images laying on top of eachother
 let _el; //default null; used to swap draggable images
 
-//var bigimagelink = ""; //1360x600 img file, may need to be global var?
-//var header = '<!DOCTYPE html>\r<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">\r<head>\r<meta charset="utf-8">\r<!-- utf-8 works for most cases -->\r<meta name="viewport" content="width=device-width">\r<!-- Forcing initial-scale shouldn\'t be necessary -->\r<meta http-equiv="X-UA-Compatible" content="IE=edge">\r<!-- Use the latest (edge) version of IE rendering engine -->\r<meta name="x-apple-disable-message-reformatting">\r<!-- Disable auto-scale in iOS 10 Mail entirely -->\r<title>WPSD Tech Corner</title>\r<!-- The title tag shows in email notifications, like Android 4.4. -->\r<!-- Web Font / @font-face : BEGIN -->\r<!-- NOTE: If web fonts are not required, lines 10 - 27 can be safely removed. -->\r<!-- Desktop Outlook chokes on web font references and defaults to Times New Roman, so we force a safe fallback font. -->\r<!--[if mso]>\r<style>\r* {font-family: sans-serif !important;}\r</style>\r<![endif]-->\r<!-- All other clients get the webfont reference; some will render the font and others will silently fail to the fallbacks. More on that here: http://stylecampaign.com/blog/2015/02/webfont-support-in-email/ -->\r<!--[if !mso]>\r<!-->\r<!-- insert web font reference, eg: <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet" type="text/css"> -->\r<!--<![endif]-->\r<!-- Web Font / @font-face : END -->\r<!-- CSS Reset : BEGIN -->\r<style>/* What it does: Remove spaces around the email design added by some email clients. */\r/* Beware: It can remove the padding / margin and add a background color to the compose a reply window. \r*/html,\rbody {margin: 0 auto !important;padding: 0 !important;height: 100% !important;width: 100% !important;}\r/* What it does: Stops email clients resizing small text. */\r* {-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;}/* What it does: Centers email on Android 4.4 */\rdiv[style*="margin: 16px 0"] {margin: 0 !important;}/* What it does: Stops Outlook from adding extra spacing to tables. */\rtable,\rtd {mso-table-lspace: 0pt !important;mso-table-rspace: 0pt !important;}/* What it does: Fixes webkit padding issue. Fix for Yahoo mail table alignment bug. Applies table-layout to the first 2 tables then removes for anything nested deeper. */\rtable {border-spacing: 0 !important;border-collapse: collapse !important;table-layout: fixed !important;margin: 0 auto !important;}\rtable table table {table-layout: auto;}/* What it does: Uses a better rendering method when resizing images in IE. */\rimg {-ms-interpolation-mode:bicubic;}/* What it does: Prevents Windows 10 Mail from underlining links despite inline CSS. Styles for underlined links should be inline. */\ra {text-decoration: none;}/* What it does: A work-around for email clients meddling in triggered links. */\r*[x-apple-data-detectors],  /* iOS */\r.unstyle-auto-detected-links *,.aBn {border-bottom: 0 !important;cursor: default !important;color: inherit !important;text-decoration: none !important;font-size: inherit !important;font-family: inherit !important;font-weight: inherit !important;line-height: inherit !important;}/* What it does: Prevents Gmail from displaying a download button on large, non-linked images. */\r.a6S {display: none !important;opacity: 0.01 !important;}/* If the above doesn\'t work, add a .g-img class to any image in question. */\rimg.g-img + div {display: none !important;}/* What it does: Removes right gutter in Gmail iOS app: https://github.com/TedGoas/Cerberus/issues/89  */\r/* Create one of these media queries for each additional viewport size you\'d like to fix */\r/* iPhone 4, 4S, 5, 5S, 5C, and 5SE */\r@media only screen and (min-device-width: 320px) and (max-device-width: 374px) {.email-container {min-width: 320px !important;}}\r/* iPhone 6, 6S, 7, 8, and X */\r@media only screen and (min-device-width: 375px) and (max-device-width: 413px) {.email-container {min-width: 375px !important;}}\r/* iPhone 6+, 7+, and 8+ */\r@media only screen and (min-device-width: 414px) {.email-container {min-width: 414px !important;}}\r</style>\r<!-- CSS Reset : END -->\r<!-- Reset list spacing because Outlook ignores much of our inline CSS. -->\r<!--[if mso]>\r<style type="text/css">\rul,ol {margin: 0 !important;}\rli {margin-left: 30px !important;}\rli.list-item-first {margin-top: 0 !important;}\rli.list-item-last {margin-bottom: 10px !important;}\r</style>\r<![endif]-->\r<!-- Progressive Enhancements : BEGIN -->\r<style>\r/* What it does: Hover styles for buttons */\r.button-td,.button-a {transition: all 100ms ease-in;}\r.button-td-primary:hover,.button-a-primary:hover {background: #661400 !important;border-color: #661400 !important;}\r/* Media Queries */\r@media screen and (max-width: 480px) {/* What it does: Forces elements to resize to the full width of their container. Useful for resizing images beyond their max-width. */\r.fluid {width: 100% !important;max-width: 100% !important;height: auto !important;margin-left: auto !important;margin-right: auto !important;}\r/* What it does: Forces table cells into full-width rows. */\r.stack-column,.stack-column-center {display: block !important;width: 100% !important;max-width: 100% !important;direction: ltr !important;}\r/* And center justify these ones. */\r.stack-column-center {text-align: center !important;}\r/* What it does: Generic utility class for centering. Useful for images, buttons, and nested tables. */\r.center-on-narrow {text-align: center !important;display: block !important;margin-left: auto !important;margin-right: auto !important;float: none !important;}\rtable.center-on-narrow {display: inline-block !important;}\r/* What it does: Adjust typography on small screens to improve readability */\r.email-container p {font-size: 17px !important;}}\r</style>\r<!-- Progressive Enhancements : END -->\r<!-- What it does: Makes background images in 72ppi Outlook render at correct size. -->\r<!--[if gte mso 9]>\r<xml>\r<o:OfficeDocumentSettings>\r<o:AllowPNG/>\r<o:PixelsPerInch>96</o:PixelsPerInch>\r</o:OfficeDocumentSettings>\r</xml>\r<![endif]-->\r</head>\r<!--The email background color (#222222) is defined in three places:1. body tag: for most email clients2. center tag: for Gmail and Inbox mobile apps and web versions of Gmail, GSuite, Inbox, Yahoo, AOL, Libero, Comcast, freenet, Mail.ru, Orange.fr3. mso conditional: For Windows 10 Mail-->\r<body width="100%" style="margin: 0; padding: 0 !important; mso-line-height-rule: exactly; background-color: #f84d00;">\r<center style="width: 100%; background-color: #f84d00;">\r<!--[if mso | IE]>\r<table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%" style="background-color: #f84d00;">\r<tr>\r<td>\r<![endif]-->\r<!-- Visually Hidden Preheader Text : BEGIN -->\r<div style="display: none; font-size: 1px; line-height: 1px; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; mso-hide: all; font-family: sans-serif;">Tech Checkup - The email thing that I try to do weekly.</div>\r<!-- Visually Hidden Preheader Text : END -->\r<!-- Create white space after the desired preview text so email clients don’t pull other distracting text into the inbox preview. Extend as necessary. -->\r<!-- Preview Text Spacing Hack : BEGIN -->\r<div style="display: none; font-size: 1px; line-height: 1px; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; mso-hide: all; font-family: sans-serif;">&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;</div>\r<!-- Preview Text Spacing Hack : END -->\r<!--Set the email width. Defined in two places:1. max-width for all clients except Desktop Windows Outlook, allowing the email to squish on narrow but never go wider than 680px.2. MSO tags for Desktop Windows Outlook enforce a 680px width.Note: The Fluid and Responsive templates have a different width (600px). The hybrid grid is more "fragile", and I\'ve found that 680px is a good width. Change with caution.-->\r<div style="max-width: 680px; margin: 0 auto;" class="email-container">\r<!--[if mso]>\r<table align="center" role="presentation" cellspacing="0" cellpadding="0" border="0" width="680">\r<tr>\r<td>\r<![endif]-->\r<!-- Email Body : BEGIN -->\r<table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="margin: 0 auto;">\r<!-- Email Header : BEGIN -->\r<tr>\r<td style="padding: 20px 0; text-align: center"><img src="http://u.cubeupload.com/XBLOssia/HeaderLogoSmall.png" width="200" height="50" alt="WPSD 1" border="0" style="height: auto; background: #dddddd; font-family: sans-serif; font-size: 15px; line-height: 15px; color: #555555;">\r</td>\r</tr>\r<!-- Email Header : END -->\r<!-- Hero Image, Flush : BEGIN -->\r<tr>\r<td style="background-color: #ffffff;">\r<img src="' + bigimagelink + '" width="680" height="" alt="alt_text" border="0" style="width: 100%; max-width: 680px; height: auto; background: #dddddd; font-family: sans-serif; font-size: 15px; line-height: 15px; color: #555555; margin: auto;" class="fluid g-img">\r</td>\r</tr>\r<!-- Hero Image, Flush : END -->';
-//look upon my works ye mighty, and despair!
-
+/*---- END GLOBAL VARS ----*/
 
 function allowDrop(ev) { //allows dropping, prevents stacking
     if (_stopdropchecker === 0) {
@@ -33,9 +32,6 @@ function isBefore(ev1, ev2) { //used for swapping
 }
 
 function dragOver(ev){ //does the swapping
-    //console.log(ev.target);
-    //console.log(ev.target.parentNode);
-    //console.log(_el);
     function cutItOut(){
         _stopdropchecker = 0;
     }
@@ -47,13 +43,6 @@ function dragOver(ev){ //does the swapping
             ev.target.parentNode.insertBefore(_el, ev.target);
         }
     }
-
-    /*if (ev.ctrlKey){
-        ev.addEventListener('dragleave', cutItOut, false);
-        var spacer = document.createElement ("p");
-        spacer.setAttribute('id', 'temp'));
-        host.appendChild (spacer);
-    }*/ //trying to make fanciness happen. It's not working.
     else{
         if (ev.ctrlKey){
         }
@@ -71,7 +60,6 @@ function drop(ev) { //Handles dropping elements, making copies
             return parseInt(number)+1;  //increment the appended # by 1
         });    //Use RegEx to increment the ID
         let copycheck = document.getElementById(newId);
-        //console.log(copycheck);
         if (copycheck == null){
         }
         while (copycheck != null){
@@ -87,18 +75,12 @@ function drop(ev) { //Handles dropping elements, making copies
         ev.target.appendChild(nodeCopy);
     }
     else ev.target.appendChild(document.getElementById(data)); //or just move that sucka
-    //_stopdropchecker = 0;
 }
 
 function dragEnd() { // DO NOT MESS WITH THIS UNLESS YOU KNOW WHY YOU'RE DOING IT, ME. - resets global vars when drag is released
     _el = null;
     _stopdropchecker = 0;
 }
-
-/*function getID(ev){
-    var oldid = ev.dataTransfer.getData("text");
-
-}*/
 
 function trash(ev) {  //removes items dropped into a trash div
     ev.preventDefault();
@@ -173,14 +155,14 @@ function checkEmpty(){ //Checks for presence of all draggables, replaces missing
             facemake.setAttribute('id', attributename);
             faceplace.appendChild(facemake);
     }
-
 }
-function makeItHtml(){
+
+function makeItHtml(){ // This one generates the form boxes - at some point I'd like to be able to account for multiple copies of an element //
     var gettags = document.getElementById('div2').querySelectorAll('img');
     var linebreak = document.createElement("p");
     //console.log(gettags);
     n = (gettags.length);
-        var kk ="";
+        let kk ="";
         for (i=0; i <= (n-1); i++){
             var list = gettags[i].id;
             var list1 = list.replace(/_(\d)+/g, "");
@@ -208,17 +190,13 @@ function makeItHtml(){
                 if (list1 === "closing"){
                     kk += '<img src="./Assets/Closing.png" height="100" width="100"><br /><form id="closing"><input type="text" name="closing1" width="800" value="Closing text 1" onKeyPress="return noEnter()"><input type="text" name="closing2" width="800" value="Closing text" onKeyPress="return noEnter()"></form><br />';
                 }
-                //console.log(kk);
         }
-        //var text = document.createTextNode(kk);
         document.getElementById("replacerator").innerHTML = kk;
-        //document.getElementById("replacerator").appendChild(text);
-        //document.getElementById("replacerator").appendChild(linebreak);
 }
-function makeItADoc(text, name, type){
+
+function makeItADoc(text, name, type){ // This one makes the HTML document! Also needs to be able to handle multiples
     var gettags = document.getElementById('div2').querySelectorAll('img');
     var linebreak = document.createElement("p");
-    //console.log(gettags);
     n = (gettags.length);
         var kk = "";
         for (i=0; i <= (n-1); i++){
